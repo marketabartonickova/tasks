aac5b21f2bd74154cffeb487cc0d785175a1ac75
Marketa Bartonickova <marketabartonickova@gmail.com>
2014-11-29 18:21:49 +0100

    prvni verze task aplikace

---
+++ app/views/tasks/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @task, :id, :name, :priority, :deadline, :created_at, :updated_at
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tasks::Application.config.secret_key_base = '20bef5a1f46b2c58454da86e1b872a08b18bb7850590cff2f471eba99dd2820be61aa2c03fb63cd2fe47553cd916f4b765ac70a8db55140b19a1891a31d87e0e'
